8438939df3431e0d6d8b8d08c163d7f47a31a37f
Nil Folquer <nfolquer@gmail.com>
2020-08-13 03:42:14 +0200

    Added infinite scroll

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const card = document.querySelector('.card');
 const modal = $('.modal')
+const endPage = document.querySelector('.endPage');
+let index = 0;
 
-card.addEventListener('click', (event) => {
-    console.log('card clicked');
-    modal.modal('toggle');
+document.addEventListener('DOMContentLoaded', (event) => {
+    for(let i = 1; i <= 12; i++) {
+        createCard(i);
+        index++;
+    }
 });
+
+document.addEventListener('scroll', (e) => {
+    const rect = endPage.getBoundingClientRect();
+    console.log(rect.top)
+    if (Math.round(rect.top) <= window.innerHeight) {
+      for(let i = 0; i < 3; i++) {
+          createCard(index);
+          index++;
+      }
+    }
+})
+
+function createCard(id) {
+    const row = document.querySelector('.row');
+
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.id = `${id}`;
+    card.style = 'width: 18rem';
+
+    const card_img = document.createElement('img');
+    card_img.className = 'card-img-top';
+    card_img.src = 'http://www.cmdperezfrias.com/blog/wp-content/uploads/2018/08/CMDPF-1200x630.jpg';
+
+    const card_body = document.createElement('div');
+    card_body.className = 'card-body';
+    card_body.id = `${id}`;
+
+    const h2 = document.createElement('h2');
+    h2.innerText = `Entreno ${id}`;
+
+    card_body.appendChild(h2);
+
+    card.appendChild(card_img);
+    card.appendChild(card_body);
+
+    row.appendChild(card);
+}
